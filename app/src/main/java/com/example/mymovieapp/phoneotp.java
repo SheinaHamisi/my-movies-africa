99aaa79384e4aeaa8721780d820d45bfeffc614d
milkaotieno <milka.otieno@student.moringaschool.com>
2022-04-27 17:05:55 +0300

    keep updates

--- app/src/main/java/com/example/mymovieapp/phoneotp.java
+++ app/src/main/java/com/example/mymovieapp/phoneotp.java
@@ -1,6 +1,6 @@
 package com.example.mymovieapp;
-import android.content.Intent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -11,7 +11,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.TaskExecutors;
 import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
