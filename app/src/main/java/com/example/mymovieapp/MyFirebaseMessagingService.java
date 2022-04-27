c85eb5cc4d5d9de37be8b38b2fafe1acc0145e07
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-04-27 08:06:05 +0300

    Edit Manifest

---
+++ app/src/main/java/com/example/mymovieapp/MyFirebaseMessagingService.java
@@ -0,0 +1,10 @@
+package com.example.mymovieapp;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    //Generate Notification
+    //Attach the notification created with custom layout
+    //Show notification
+}
