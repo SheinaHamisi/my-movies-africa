d33c42ea063c85ffc07830b6c345d5496ddb24d7
Ephraim19 <machayoephraim@gmail.com>
2022-04-27 10:02:18 +0300

    Implemented random method to randomize movies

--- app/src/main/java/com/example/mymovieapp/MainActivity.java
+++ app/src/main/java/com/example/mymovieapp/MainActivity.java
@@ -3,12 +3,51 @@ package com.example.mymovieapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.mymovieapp.databinding.ActivityMainBinding;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    List<Integer> givenList = Arrays.asList(R.drawable.movies_1,R.drawable.movies_2,R.drawable.movies_3,R.drawable.movies_4,R.drawable.movies_5);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        binding.randomize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random random = new Random();
+                Integer randomElement = givenList.get(random.nextInt(givenList.size()));
+                Toast.makeText(MainActivity.this,randomElement.toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
