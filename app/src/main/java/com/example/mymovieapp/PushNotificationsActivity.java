d4ba2a230505064528be531767d1df5df92a5601
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-04-27 07:43:22 +0300

    Add push notifications activity

---
+++ app/src/main/java/com/example/mymovieapp/PushNotificationsActivity.java
@@ -0,0 +1,14 @@
+package com.example.mymovieapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PushNotificationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_push_notifications);
+    }
+}
