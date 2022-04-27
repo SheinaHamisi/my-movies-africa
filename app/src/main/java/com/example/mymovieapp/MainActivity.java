d2f085448472850c290c0667bfa58d27f2f20f8b
Ephraim19 <machayoephraim@gmail.com>
2022-04-27 11:39:38 +0300

    Final project

--- app/src/main/java/com/example/mymovieapp/MainActivity.java
+++ app/src/main/java/com/example/mymovieapp/MainActivity.java
@@ -1,24 +1,24 @@
 package com.example.mymovieapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import com.example.mymovieapp.databinding.ActivityMainBinding;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     RecyclerView recyclerView;
     List<Integer> givenList = Arrays.asList(R.drawable.movies_1,R.drawable.movies_2,R.drawable.movies_3,R.drawable.movies_4,R.drawable.movies_5);
+    LinearLayoutManager HorizontalLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = binding.recyclerView;
         MoviesRecycler adapter = new MoviesRecycler(this,givenList);
         recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        HorizontalLayout = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(HorizontalLayout);
 
         binding.randomize.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,11 +40,22 @@ public class MainActivity extends AppCompatActivity {
                 Collections.shuffle(givenList);
                 MoviesRecycler adapter = new MoviesRecycler(MainActivity.this,givenList);
                 recyclerView.setAdapter(adapter);
-                recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+                HorizontalLayout = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false);
+                recyclerView.setLayoutManager(HorizontalLayout);
             }
         });
     }
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
