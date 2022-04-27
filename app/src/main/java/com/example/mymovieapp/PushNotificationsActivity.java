82175313e7f4e7686755b33feb21c5291cfc97ab
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-04-27 13:04:08 +0300

    Add push notification

--- app/src/main/java/com/example/mymovieapp/PushNotificationsActivity.java
+++ app/src/main/java/com/example/mymovieapp/PushNotificationsActivity.java
@@ -3,12 +3,18 @@ package com.example.mymovieapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.mymovieapp.databinding.ActivityPushNotificationsBinding;
 
 public class PushNotificationsActivity extends AppCompatActivity {
 
+    private ActivityPushNotificationsBinding activityPushNotificationsBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_push_notifications);
+        activityPushNotificationsBinding = ActivityPushNotificationsBinding.inflate(getLayoutInflater());
+        View view = activityPushNotificationsBinding.getRoot();
+        setContentView(view);
     }
 }
