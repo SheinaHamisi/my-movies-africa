30b824b27169018878716c506e17d44352c3a263
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-04-28 15:56:55 +0300

    Add Retrieve Function

--- app/src/main/java/com/example/mymovieapp/PushNotificationsActivity.java
+++ app/src/main/java/com/example/mymovieapp/PushNotificationsActivity.java
@@ -3,12 +3,15 @@ package com.example.mymovieapp;
 import static android.content.ContentValues.TAG;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
 import com.example.mymovieapp.databinding.ActivityPushNotificationsBinding;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -19,6 +22,9 @@ public class PushNotificationsActivity extends AppCompatActivity {
 
     private ActivityPushNotificationsBinding activityPushNotificationsBinding;
     RecyclerAdapter recyclerAdapter;
+    private RecyclerView recyclerView;
+    RecyclerAdapter adapter;
+    DatabaseReference mbase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,19 +33,38 @@ public class PushNotificationsActivity extends AppCompatActivity {
         View view = activityPushNotificationsBinding.getRoot();
         setContentView(view);
 
-        //recycler Constructor
-        recyclerAdapter = new RecyclerAdapter();
-        activityPushNotificationsBinding.recyclerView.setAdapter(recyclerAdapter);
-        initUpdateNotification();//Calling the method
+        mbase = FirebaseDatabase.getInstance().getReference();
 
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(
+                new LinearLayoutManager(this));
 
+        // It is a class provide by the FirebaseUI to make a
+        // query in the database to fetch appropriate data
+        FirebaseRecyclerOptions<NotificationModel> options
+                = new FirebaseRecyclerOptions.Builder<NotificationModel>()
+                .setQuery(mbase, NotificationModel.class)
+                .build();
+        // Connecting object of required Adapter class to
+        // the Adapter class itself
+        adapter = new RecyclerAdapter(options);
+        // Connecting Adapter class with the Recycler view*/
+        recyclerView.setAdapter(adapter);
     }
 
-    private void initUpdateNotification() {
-        List<String> notificationName = new ArrayList<>();
-        notificationName.add("Test");
-        recyclerAdapter.updateNotificationName(notificationName);
+    // Function to tell the app to start getting
+    // data from database on starting of the activity
+    @Override protected void onStart()
+    {
+        super.onStart();
+        adapter.startListening();
     }
 
-
+    // Function to tell the app to stop getting
+    // data from database on stopping of the activity
+    @Override protected void onStop()
+    {
+        super.onStop();
+        adapter.stopListening();
+    }
 }
--- app/src/main/java/com/example/mymovieapp/RecyclerAdapter.java
+++ app/src/main/java/com/example/mymovieapp/RecyclerAdapter.java
@@ -1,58 +1,69 @@
 package com.example.mymovieapp;
 
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mymovieapp.databinding.RecyclerRowBinding;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder>{
-    List<String> notificationName = new ArrayList<>();
+public class RecyclerAdapter extends FirebaseRecyclerAdapter<
+        NotificationModel, RecyclerAdapter.notificationViewholder> {
 
-    //method to update disease names
-    public void updateNotificationName(List<String> notificationName){
-        this.notificationName = notificationName;
+    public RecyclerAdapter(
+            @NonNull FirebaseRecyclerOptions<NotificationModel> options)
+    {
+        super(options);
     }
 
 
+    @Override
+    protected void
+    onBindViewHolder(@NonNull notificationViewholder holder,
+                     int position, @NonNull NotificationModel model)
+    {
+
+        holder.title.setText(model.getTitle());
+
+        holder.text.setText(model.getText());
+
+    }
+
+    // Function to tell the class about the Card view (here
+    // "person.xml")in
+    // which the data will be shown
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        RecyclerRowBinding recyclerRowBinding = RecyclerRowBinding.inflate(layoutInflater, parent, false);
-        return new ViewHolder(recyclerRowBinding);
-
+    public notificationViewholder
+    onCreateViewHolder(@NonNull ViewGroup parent,
+                       int viewType)
+    {
+        View view
+                = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recycler_row, parent, false);
+        return new RecyclerAdapter.notificationViewholder(view);
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.bindView(notificationName.get(position));
-    }
 
-    @Override
-    public int getItemCount() {
-        return notificationName.size();
-    }
+    class notificationViewholder
+            extends RecyclerView.ViewHolder {
+        TextView title, text;
+        public notificationViewholder(@NonNull View itemView)
+        {
+            super(itemView);
 
-    //View holder
-    class ViewHolder extends RecyclerView.ViewHolder {
+            title
+                    = itemView.findViewById(R.id.title);
+            text = itemView.findViewById(R.id.text);
 
-        //Constructor for the ViewHolder
-        private RecyclerRowBinding recyclerRowBinding;
-
-        public ViewHolder(@NonNull RecyclerRowBinding recyclerRowBinding){
-            super(recyclerRowBinding.getRoot());
-            this.recyclerRowBinding = recyclerRowBinding;
         }
-        //Bind object to recycler view
-        public void bindView(String notificationName){
-            recyclerRowBinding.notificationTextView.setText(notificationName);
-        }
-
     }
 }
