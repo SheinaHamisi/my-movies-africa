b1df9abd2cea24bc4b9e6a13105861f74ea7e075
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-05-05 16:12:24 +0300

    Fetch data from Real-time DB

--- app/src/main/java/com/example/mymovieapp/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/mymovieapp/MyFirebaseMessagingService.java
@@ -8,7 +8,9 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Looper;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
@@ -16,21 +18,38 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import com.example.mymovieapp.databinding.ActivityPushNotificationsBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private ActivityPushNotificationsBinding activityPushNotificationsBinding;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    NotificationModel notificationModel;
+
 
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onMessageReceived(@NonNull RemoteMessage message) {
         String title = message.getNotification().getTitle();
+        Looper.prepare();
+        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
         String text = message.getNotification().getBody();
         final String CHANNEL_ID = "HEADS_UP_NOTIFICATION";
 
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Notification");
+        notificationModel = new NotificationModel();
+
+
+        addDatatoFirebase(title, text);
         NotificationChannel channel = new NotificationChannel(
                 CHANNEL_ID,"Heads Up Notification", NotificationManager.IMPORTANCE_HIGH
         );
@@ -43,5 +62,26 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         NotificationManagerCompat.from(this).notify(1, notification.build());
 
         super.onMessageReceived(message);
+
+
+    }
+
+    private void addDatatoFirebase(String title, String text) {
+        notificationModel.setTitle(title);
+        notificationModel.setText(text);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                databaseReference.setValue(notificationModel);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 }
