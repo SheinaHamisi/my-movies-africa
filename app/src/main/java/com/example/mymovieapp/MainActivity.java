8a472a763cc57218132f14b209ac8b82fb9bad7c
SheinaHamisi <hamisisheina@gmail.com>
2022-05-06 11:17:34 +0300

    added image slider

--- app/src/main/java/com/example/mymovieapp/MainActivity.java
+++ app/src/main/java/com/example/mymovieapp/MainActivity.java
@@ -7,17 +7,35 @@ import android.view.View;
 
 import com.example.mymovieapp.databinding.ActivityMainBinding;
 
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
 public class MainActivity extends AppCompatActivity {
+    SliderView sliderView;
+    int[] images = {R.drawable.one,
+            R.drawable.two,
+            R.drawable.three,
+            R.drawable.four,
+            R.drawable.five,
+            R.drawable.six};
 
 
     private ActivityMainBinding activityMainBinding;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        View view = activityMainBinding.getRoot();
-        setContentView(view);
+        setContentView(R.layout.activity_main);
+
+        sliderView = findViewById(R.id.image_slider);
+
+        SliderAdapter sliderAdapter = new SliderAdapter(images);
+
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
+
     }
 }
